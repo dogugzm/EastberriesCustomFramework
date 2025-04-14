9e31b76b39d3543e5fe19bb4bafe5799aced5cbd
Doğukan Gözüm <dogukangozum@hotmail.com>
2025-04-14 22:26:31 +0300

    panel service added

---
+++ Panel Service/Runtime/PanelFactory.cs
@@ -0,0 +1,26 @@
+﻿using VContainer.Unity;
+using UnityEngine;
+using VContainer;
+
+namespace Assets.Scripts.PanelService
+{
+    public interface IPanelFactory
+    {
+        IPanel CreatePanel(GameObject prefab);
+    }
+
+    public class PanelFactory : IPanelFactory
+    {
+        private readonly IObjectResolver _container;
+
+        public PanelFactory(IObjectResolver container)
+        {
+            _container = container;
+        }
+
+        public IPanel CreatePanel(GameObject prefab)
+        {
+            return _container.Instantiate(prefab).GetComponent<IPanel>();
+        }
+    }
+}
